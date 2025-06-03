48a58b208137265e7e39be1cf3478d417cee265c
Stylianos Didaskalou <steliosdidaskalou@hotmail.com>
2025-06-03 11:18:15 +0300

    Updated the list with exercises

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -4,8 +4,32 @@ import matplotlib.pyplot as plt
 class DEFAULT_SETTINGS: 
     def exercises_list():
         list = []
-        list.append('Leg raises')
-        list.append('Push-ups')
+        list.append('Calf_Pumps')
+        list.append('Knee_Extensions')
+        list.append('Seated_Marching')
+        list.append('Ankle_circles')
+        list.append('Heel_Slides')
+        list.append('Supine_Leg_Lifts')
+        list.append('Side_Leg_Lifts')
+        list.append('Clamshells')
+        list.append('Glute_Bridges')
+        list.append('Leg_Abductions')
+        list.append('F_Bicycle_Both_Legs')
+        list.append('F_Supine_Bicycle_Both_Legs')
+        list.append('F_Bicycle_Reverse_Leg_Left')
+        list.append('F_Bicycle_Reverse_Leg_Right')
+        list.append('F_Prone_Alternate')
+        list.append('F_Prone_Alternate_Knee_Bending')
+        list.append('F_Prone_Both_Legs')
+        list.append('F_Single_Leg_Jumps_Left')
+        list.append('F_Single_Leg_Jumps_Right')
+        list.append('F_Supine_Bend_Knee_Ankle_Rotation')
+        list.append('F_Supine_Bend_Knee_Ankle_Dorsi_Plantar_Flexion')
+        list.append('F_Supine_Bicycle_Inner_Leg')
+        list.append('F_Supine_Reverse_Straight_Leg_Left')
+        list.append('F_Supine_Reverse_Straight_Leg_Right')
+        list.append('F_Supine_Straight_Legs_Ankle_Rotation')
+        list.append('F_Supine_Straight_Legs_Ankle_Dorsi_Plantar_Flexion')
         return list
 
     def max_cameras(): # the maximum number of cameras that can be connected
